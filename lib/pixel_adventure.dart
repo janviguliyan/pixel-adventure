52464c98480ada8ffaf8b7df437c9211c6adb7a6
janviguliyan <JANVIGULIYAN@GMAIL.COM>
2024-04-06 00:55:33 +0530

    update: basic game setup

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -1,5 +1,28 @@
+import 'dart:async';
+import 'dart:ui';
+
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:pixel_adventure/levels/level.dart';
+// import 'dart:ui';
 
-class PixelAdventure extends FlameGame{
+class PixelAdventure extends FlameGame {
+  @override
+  Color backgroundColor() => const Color(0xFF211F30);
+  late final CameraComponent cam;
+  final myWorld = Level();
 
+  @override
+  FutureOr<void> onLoad() async {
+    //loading images into cache
+    await images.loadAllImages();
+
+    cam = CameraComponent.withFixedResolution(
+        world: myWorld, width: 640, height: 360);
+    cam.viewfinder.anchor = Anchor.topLeft;
+
+    addAll([cam, myWorld]);
+
+    return super.onLoad();
+  }
 }
